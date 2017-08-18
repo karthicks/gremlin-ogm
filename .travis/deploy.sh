10c7ea386b9271108d4bc8e2088cdb1cf1c5133e
Karthick Sankarachary <karthick@elementum.com>
2017-08-18 08:44:53 -0700

    TINKERPOP-1750 Prepare for releasing 3.3.0-RC1 to Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -23,6 +23,9 @@ if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    openssl aes-256-cbc -K $encrypted_a05ded3ed24d_key -iv $encrypted_a05ded3ed24d_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    gpg --fast-import .travis/codesigning.asc
+    rm .travis/codesigning.asc
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
